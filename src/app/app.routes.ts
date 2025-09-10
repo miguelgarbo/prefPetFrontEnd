2903bffba79c2cef09379c2f68cce186be2cb556
miguelgarbo <miguelggarbo@gmail.com>
2025-09-10 17:04:08 -0300

    Rotas Bases Adicionadas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import path from 'path';
+import { MenuComponent } from './components/menu/menu.component';
+import { LoginComponent } from './components/login/login.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { CadastroUsuarioComponent } from './components/cadastro-usuario/cadastro-usuario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+{path: '', redirectTo:'principal', pathMatch:'full'},
+{path:'login', component: LoginComponent},
+{path:'cadastro', component: CadastroUsuarioComponent},
+{path:'principal', component: PrincipalComponent, 
+    children:[
+        {path:"menu", component:MenuComponent},
+    ]
+}
+
+];
