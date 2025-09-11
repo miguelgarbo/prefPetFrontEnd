0bab2de6bf83426b268f9f4969c45dd7b05dc10e
Mariana <mariana.sallina@gmail.com>
2025-09-11 08:22:45 -0300

    tela de login base

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { Router, Routes } from '@angular/router';
+
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [MdbFormsModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  usuario!: string;
+  senha!: string;
+
+  router = inject(Router);
+
+  logar(){
+    if(this.usuario == 'admin' && this.senha == '12345'){
+      this.router.navigate(['principal']);
+    }else{
+      alert("USUÁRIO E/OU SENHA INCORRETOS!!!");
+    }
+  }
 }
