7e2e14f26b75b457635ff3a6b6e7a2c1dbf518c9
Mariana <mariana.sallina@gmail.com>
2025-09-11 13:52:27 -0300

    modal login finalizado

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import { MenuComponent } from './components/menu/menu.component';
 import { LoginComponent } from './components/login/login.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { CadastroUsuarioComponent } from './components/cadastro-usuario/cadastro-usuario.component';
+import { InicialComponent } from './components/inicial/inicial.component';
 
 export const routes: Routes = [
 {path: '', redirectTo:'principal', pathMatch:'full'},
 {path:'login', component: LoginComponent},
 {path:'cadastro', component: CadastroUsuarioComponent},
+{path: 'inicial', component: InicialComponent},
 {path:'principal', component: PrincipalComponent, 
     children:[
         {path:"menu", component:MenuComponent},
