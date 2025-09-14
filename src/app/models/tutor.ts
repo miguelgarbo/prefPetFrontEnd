0604aa5433b6bd8e72cb11447f02fc40468ed6c4
miguelgarbo <miguelggarbo@gmail.com>
2025-09-14 12:54:36 -0300

    Tutor Service

--- src/app/models/tutor.ts
+++ src/app/models/tutor.ts
@@ -1,2 +1,18 @@
+import { Animal } from "./animal";
+
 export class Tutor {
+
+    
+        id!: number;
+        nome!: string;
+        senha!: string;
+        email!: string;
+        telefone!: string;
+        cpf!: string;
+        cnpj!: string;
+        cep!: string;
+        cidade!: string;
+        estado!:string;
+        animais!: Animal[]
+        imagemUrlPerfil!: string;
 }
