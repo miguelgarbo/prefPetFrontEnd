9177f0880b64a8730e59a5e0c31a7ace453135d2
Mariana <mariana.sallina@gmail.com>
2025-09-17 13:59:17 -0300

    NavBar 2 foto e nome

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Nav1Component } from "./components/nav1/nav1.component";
+import { Nav2Component } from "./components/nav2/nav2.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Nav1Component],
+  imports: [RouterOutlet, Nav1Component, Nav2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
