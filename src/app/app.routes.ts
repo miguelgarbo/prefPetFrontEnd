5aa4c3bfde98aeca7b6c6342c6324f1140d59a24
Mariana <mariana.sallina@gmail.com>
2025-09-17 21:05:03 -0300

    Segunda NavBar com 3 opções funcionando

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,18 +3,15 @@ import { LoginComponent } from './components/login/login.component';
 import { PrincipalComponent } from './components/principal/principal.component';
 import { CadastroUsuarioComponent } from './components/cadastro-usuario/cadastro-usuario.component';
 import { PublicacaoListComponent } from './components/publicacao-list/publicacao-list.component';
-import { NavBarPublicacaoComponent } from './components/nav-bar-publicacao/nav-bar-publicacao.component';
 import { InicialComponent } from './components/inicial/inicial.component';
 import { CadastroPublicacaoComponent } from './components/cadastro-publicacao/cadastro-publicacao.component';
-import { Nav1Component } from './components/nav1/nav1.component';
-import { Nav2Component } from './components/nav2/nav2.component';
 import { CarteiraVacinacaoComponent } from './components/carteira-vacinacao/carteira-vacinacao.component';
 import { EmergenciaComponent } from './components/emergencia/emergencia.component';
 import { BuscarChipComponent } from './components/buscar-chip/buscar-chip.component';
 
 
 export const routes: Routes = [
-{path: '', redirectTo:'carteira-vacinacao', pathMatch:'full'},
+{path: '', redirectTo:'inicial', pathMatch:'full'},
 {path: "cadastro-publicacao", component: CadastroPublicacaoComponent},
 {path: 'publicacoes', component: PublicacaoListComponent},
 {path:'login', component: LoginComponent},
