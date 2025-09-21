3a6ee5cdf589a5827c80bd209b5d25f714b52949
Ja1Rf <joaoreisfelini@gmail.com>
2025-09-21 08:04:49 -0300

    ADD: modal para cadastro e integracao BD

--- src/app/services/animal.service.ts
+++ src/app/services/animal.service.ts
@@ -4,30 +4,33 @@ import { Observable } from 'rxjs';
 import { Animal } from '../models/animal';
 
 @Injectable({
-  providedIn: 'root'  
+  providedIn: 'root'
 })
 
 export class AnimalService {
-  private apiUrl = 'http://localhost:8080/animais'; 
+  private apiUrl = 'http://localhost:8080/animais';
 
   private http = inject(HttpClient);
 
   findAll(): Observable<Animal[]> {
-    return this.http.get<Animal[]>(this.apiUrl+"/findAll");
+    return this.http.get<Animal[]>(this.apiUrl + "/findAll");
   }
 
-    findByTutor(tutorId: number): Observable<Animal[]> {
-      return this.http.get<Animal[]>(`${this.apiUrl}/findByTutor?tutorId=${tutorId}`);
-    }
-
-  findByMicrochip(numeroMicrochip: string): Observable<Animal>{
-    return this.http.get<Animal>(this.apiUrl+"/findByMicrochip",{
-        params: {numeroMicrochip: numeroMicrochip}
-      } )
+  findById(id: number): Observable<Animal> {
+    return this.http.get<Animal>(`${this.apiUrl}/findById/${id}`)
   }
 
-  findById(id:number): Observable<Animal>{
-      return this.http.get<Animal>(this.apiUrl+"/findById/"+id)
-    }
+  findByTutor(tutorId: number): Observable<Animal[]> {
+    return this.http.get<Animal[]>(`${this.apiUrl}/findByTutor?tutorId=${tutorId}`);
+  }
 
+  findByMicrochip(numeroMicrochip: string): Observable<Animal> {
+    return this.http.get<Animal>(this.apiUrl + "/findByMicrochip", {
+      params: { numeroMicrochip: numeroMicrochip }
+    })
+  }
+
+  save(animal: Partial<Animal>): Observable<Animal> {
+    return this.http.post<Animal>(`${this.apiUrl}/save`, animal);
+  }
 }
