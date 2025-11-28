137f9235bab0182f40b993d6784db2194ce15c06
moroniii <moronidemelo@gmail.com>
2025-11-28 20:50:54 -0300

    Vacinas: novas abas, classificação e integração com backend

--- src/app/components/vacina/vacinas/vacinas.component.ts
+++ src/app/components/vacina/vacinas/vacinas.component.ts
@@ -1,22 +1,18 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { MdbTabsModule } from 'mdb-angular-ui-kit/tabs';
-import { RouterOutlet, Router } from '@angular/router';
-
 import { AnimalService } from '../../../services/animal.service';
+import { Animal } from '../../../models/animal';
 import { AplicacaoVacinaService } from '../../../services/aplicacao-vacina.service';
+import { Router } from '@angular/router';
 import { TutorService } from '../../../services/tutor.service';
 import { LoginService } from '../../../services/login.service';
-
-import { Animal } from '../../../models/animal';
 import { Usuario } from '../../../models/usuario';
-import { AplicacaoVacina } from '../../models/aplicacao-vacina';
-
 import Swal from 'sweetalert2';
 import { VacinaslistComponent } from '../vacinaslist/vacinaslist.component';
 
 @Component({
   selector: 'app-vacinas',
-  imports: [MdbTabsModule, RouterOutlet, VacinaslistComponent],
+  imports: [ MdbTabsModule, VacinaslistComponent ],
   templateUrl: './vacinas.component.html',
   styleUrl: './vacinas.component.scss'
 })
@@ -31,7 +27,6 @@ export class VacinasComponent implements OnInit {
 
   // Dados
   pets: Animal[] = [];
-  aplicacoes: AplicacaoVacina[] = [];
 
   // ID do animal selecionado → usado no VacinaslistComponent
   animalIdCapturado: number = 0;
@@ -43,52 +38,35 @@ export class VacinasComponent implements OnInit {
     this.findAnimaisByTutorId(this.currentUser.id);
   }
 
-  // ---- Buscar animais do tutor ----
   findAnimaisByTutorId(id: number) {
     this.animalService.findByTutorId(id).subscribe({
       next: (animais) => {
         this.pets = animais;
 
-        console.log("Animais do tutor:", animais);
-
+        // Seleciona automaticamente o primeiro pet
         if (this.pets.length > 0) {
-          this.findById(this.pets[0].id); // seleciona o primeiro automaticamente
+          this.findById(this.pets[0].id);
         }
       },
-      error: (err) => console.error(err)
+      error: err => console.error(err)
     });
   }
 
-  // ---- Selecionar animal clicado ----
   findById(id: number) {
     this.animalService.findById(id).subscribe({
       next: (animal) => {
-        console.log("Animal selecionado:", animal);
         this.animalIdCapturado = animal.id;
-        this.findByAnimal();
       },
-      error: (err) => console.error("Erro ao pegar animal", err)
+      error: err => console.error("Erro ao pegar animal", err)
     });
   }
 
-  // ---- Carregar aplicações do animal ----
-  findByAnimal() {
-    this.aplicacaoService.findByAnimalId(this.animalIdCapturado).subscribe({
-      next: (aplicacoes) => {
-        console.log("Aplicações:", aplicacoes);
-        this.aplicacoes = aplicacoes;
-      },
-      error: (err) => console.error(err)
-    });
-  }
-
-  // ---- Gerar PDF ----
   gerarPdf() {
     if (this.animalIdCapturado) {
       this.router.navigate(['principal/carteira-vacinacao', this.animalIdCapturado]);
     } else {
       Swal.fire({
-        title: "Selecione um Animal Para Gerar o Pdf",
+        title: "Selecione um animal para gerar o PDF",
         icon: "warning",
         confirmButtonText: 'Ok'
       });
